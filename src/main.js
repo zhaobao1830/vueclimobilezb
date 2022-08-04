b91fc24d8eb5cadb31caeb3b5d8b0cb6f43524ab
zhaobao1830 <373098777@qq.com>
2022-08-04 15:54:10 +0800

    删除main.js页面 button组件的引入

--- src/main.js
+++ src/main.js
@@ -6,14 +6,11 @@ import lazyPlugin from 'vue3-lazy'
 import loadingDirective from '@/components/base/loading/directive'
 import noResultDirective from '@/components/base/no-result/directive'
 
-import { Button } from 'vant'
-
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
 
 const app = createApp(App)
 
 app.use(store).use(router)
-  .use(Button)
   .use(lazyPlugin, { loading: require('@/assets/images/default.png') })
   .directive('loading', loadingDirective).directive('no-result', noResultDirective).mount('#app')
