c469485a28f827d67116f753a0b5853a3023ee16
zhaobao1830 <373098777@qq.com>
2022-09-23 17:46:28 +0800

    新加ruler刻度尺组件

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="home">
     <van-button type="primary">我是首页按钮</van-button>
+    <div class="ruler-demo">
+      当前值：{{value}}
+      <ruler-component
+        v-model="value"
+        :scope="scope"
+        :step="10"
+        :unit="1"
+      ></ruler-component>
+    </div>
   </div>
 </template>
 
 <script setup>
+  import RulerComponent from '@/components/base/Ruler/RulerComponent'
   import { useStore } from 'vuex'
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
 
   const store = useStore()
   const playlist = computed(() => store.getters.currentSong)
 
   console.log(playlist)
+
+  const value = ref(10)
+  const scope = ref([0, 30])
 </script>
 
 <style scoped lang="scss">
-
+  .ruler-demo{
+    width: 360px;
+  }
 </style>
